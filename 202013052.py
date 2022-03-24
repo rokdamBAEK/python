ae1af70d858caed060e8533eee4e1614bb16cc87
rokdamBAEK <102267910+rokdamBAEK@users.noreply.github.com>
2022-03-24 09:40:10 +0900

    Add files via upload

---
+++ 202013052.py
@@ -0,0 +1,45 @@
+print("hello world")
+
+print("Mary's cosmetics")
+
+print('신씨가 소리질렀다. "도둑이야".')
+
+print('"C:\windows"')
+
+print("안녕하세요. \n만너서\t\t반갑습니다.") #\n 은 줄내림 \t는 tap처럼 뛰우기
+
+movie_rank = ["닥터 스트레인지", "스플릿", "럭키"]
+
+print(movie_rank)
+
+movie_rank.insert(3, '배트맨')
+
+print(movie_rank)
+
+movie_rank.insert(1, '슈퍼맨')\
+
+print(movie_rank)
+
+nums = [1,2,3,4,5]
+rusult = sum(nums)
+
+print(rusult)
+
+cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "소시지", "라면", "팥빙수", "김치전"]
+con = len(cook)
+print(con)
+
+#nuns 변수 위에서 선언함 nums = [1,2,3,4,5]
+pop = len(nums)  # 요소 수 받아오기
+pop2  = rusult / pop # 총합 / 요소수 = 평균값
+print(pop2)
+
+print(3==5) # 연산자 == 절대 같다인데 3 == 5은 논리적으로 말이 안되서 False 값이 뜸
+
+print(3<5) # 연산자 3<5는 5가 크다니는 거니까 논리적으로 맞아서 True가 나옴
+
+x=4
+print(1<x<5) #변수 x가 4로 선언해줘서 1보다는 크고 5보다는 작아서 True값 나옴
+
+print((3==3) and (4!=3))  # ==인 비교 연산자는 무조건 값이 같아야 하고 != 비교 연산자는 무조건 값이 달라야해서 두 연산 모두 충족하니까 True가 나옴
+
